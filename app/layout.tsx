ed0b20b44f8589efbf8b39d702ebc38e7cee52ca
DrCloy <alsry0712@gmail.com>
2024-03-01 15:25:06 +0900

    Git Init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from "next";
+import Navigation from "../components/nagivation";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Next Movies",
+  },
+  description: "Ths best movies on the best framework",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
+}
