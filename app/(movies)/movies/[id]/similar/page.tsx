22a6d67a4b7b873ab10a7c784d3c04586a62b98c
DrCloy <alsry0712@gmail.com>
2024-03-05 00:21:37 +0900

    Create Similar page and Adjust movie component

--- app/(movies)/movies/[id]/similar/page.tsx
+++ app/(movies)/movies/[id]/similar/page.tsx
@@ -1,5 +1,8 @@
+import { API_URL } from "../../../../(home)/page";
+import Movie from "../../../../../components/movie";
 import { getMovie } from "../../../../../components/movie-info";
 import { IParams } from "../page";
+import styles from "../../../../../styles/similars.module.css";
 
 export async function generateMetadata({ params: { id } }: IParams) {
   const movie = await getMovie(id);
@@ -9,6 +12,24 @@ export async function generateMetadata({ params: { id } }: IParams) {
   };
 }
 
-export default function Similar() {
-  return <div>Similar</div>;
+async function getSimilarMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}/similar`);
+
+  return response.json();
+}
+
+export default async function Similar({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  const similarMovie = await getSimilarMovie(id);
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>{`Similar Movie - ${movie.title}`}</div>
+      <hr />
+      <div className={styles.movie}>
+        {similarMovie.map((movie) => (
+          <Movie key={movie.id} title={movie.title} id={movie.id} poster_path={movie.poster_path} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- components/movie.tsx
+++ components/movie.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import Link from "next/link";
 import styles from "../styles/movie.module.css";
-import { useRouter } from "next/navigation";
 
 interface IMovieProps {
   title: string;
@@ -11,14 +8,14 @@ interface IMovieProps {
 }
 
 export default function Movie({ title, id, poster_path }: IMovieProps) {
-  const router = useRouter();
-  const onClick = () => {
-    router.push(`/movies/${id}`);
-  };
   return (
     <div className={styles.movie}>
-      <img src={poster_path} alt={title} onClick={onClick} />
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <Link href={`/movies/${id}`}>
+        <div className={styles.image}>
+          <img src={poster_path} alt={title} />
+        </div>
+        <div className={styles.title}>{title}</div>
+      </Link>
     </div>
   );
 }
